9f868c2d0dc5220758515bd15dcf82c2631204b1
reich452 <reich452@regis.edu>
2017-04-02 13:26:55 -0600

    Added network controller

---
+++ BingImageSearch/ImageController .swift
@@ -0,0 +1,31 @@
+//
+//  ImageController .swift
+//  BingImageSearch
+//
+//  Created by Nick Reichard on 4/2/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageController {
+    
+    static func image(forURL url: String, completion: @escaping (UIImage?) -> Void) {
+        
+        guard let url = URL(string: url) else {
+            fatalError("Image URL optional is nil")
+        }
+        
+        NetworkController.performRequest(for: url, httpMethod: .Get) { (data, error) in
+            guard let data = data,
+                let image = UIImage(data: data) else {
+                    
+                    DispatchQueue.main.async { completion(nil) }
+                    return
+            }
+            DispatchQueue.main.async { completion(image) }
+            
+        }
+    }
+}
